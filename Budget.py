f7ff2199bed1809ee813dad4ee7fe56f4aaa3cbd
Psami-wondah <okechukwusamuel16@gmail.com>
2021-04-12 21:19:53 +0100

    added an initializing function and created some loop backs. Exceptions not handled yet

--- Budget.py
+++ Budget.py
@@ -1,3 +1,4 @@
+Samuel = False
 class Budget:
     def __init__(self, food, clothing, entertainment):
         self.food = food
@@ -12,12 +13,12 @@ class Budget:
     def depositFunction(self, category):
         depositAmount = int(input('How much would you like to deposit? \n'))
         self.balance[category] += depositAmount
-        print('%d Naira has Been added to Food and now the balance is: %d' % (depositAmount, self.balance[category]))
+        print(f'%d Naira has Been added to {self.food} and now the balance is: %d' % (depositAmount, self.balance[category]))
         selectedOption = int(input('Would you like to deposit into any other account? (1) Yes (2) No \n'))
         if selectedOption == 1:
             self.deposit()
         else:
-            pass
+            init()
 
     def withdrawalFunction(self, category):
         withdrawalAmount = int(input('How much would you like to withdraw? \n'))
@@ -31,16 +32,22 @@ class Budget:
         if selectedOption == 1:
             self.withdraw()
         else:
-            pass
+            init()
 
     def tranferOperation(self, first, second):
-        selectedOption=int(input('Enter the amount you wish to transfer \n'))
-        self.balance[first] -= selectedOption
-        self.balance[second] += selectedOption
-        if selectedOption <= self.balance[first]:
+        transferAmount=int(input('Enter the amount you wish to transfer \n'))
+
+        if transferAmount <= self.balance[first]:
+            self.balance[first] -= transferAmount
+            self.balance[second] += transferAmount
             print(f'Transfer done and now {first} is: {self.balance[first]} and \n {second} is {self.balance[second]}')
         else:
             print('Sorry, you do not have enough funds.')
+        selectedOption = int(input('Would you like to make any other transfer? ? (1) Yes (2) No \n'))
+        if selectedOption == 1:
+            self.transfer()
+        else:
+            init()
 
     def deposit(self):
         print('Which category do you wish to deposit into?')
@@ -77,10 +84,10 @@ class Budget:
         print(f'1. {self.food}')
         print(f'2. {self.clothing}')
         print(f'3. {self.entertainment}')
-        print(f'4. {self.food} + {self.clothing}')
-        print(f'5. {self.food} + {self.entertainment}')
-        print(f'6. {self.clothing} + {self.entertainment}')
-        print(f'7. Total Balance')
+        # print(f'4. {self.food} + {self.clothing}')
+        # print(f'5. {self.food} + {self.entertainment}')
+        # print(f'6. {self.clothing} + {self.entertainment}')
+        print(f'4. Total Balance')
         selectedOption = int(input('Please select an option \n'))
         if selectedOption == 1:
             print(f'The balance is: {self.balance[self.food]}')
@@ -88,17 +95,22 @@ class Budget:
             print(f'The balance is: {self.balance[self.clothing]}')
         elif selectedOption == 3:
             print(f'The balance is: {self.balance[self.entertainment]}')
+        # elif selectedOption == 4:
+        #     print('The balance is: ', self.balance[self.food] + self.balance[self.clothing])
+        # elif selectedOption == 5:
+        #     print('The balance is: ', self.balance[self.food] + self.balance[self.entertainment])
+        # elif selectedOption == 6:
+        #     print('The balance is: ', self.balance[self.entertainment] + self.balance[self.clothing])
         elif selectedOption == 4:
-            print('The balance is: ', self.balance[self.food] + self.balance[self.clothing])
-        elif selectedOption == 5:
-            print('The balance is: ', self.balance[self.food] + self.balance[self.entertainment])
-        elif selectedOption == 6:
-            print('The balance is: ', self.balance[self.entertainment] + self.balance[self.clothing])
-        elif selectedOption == 7:
             print('The balance is: ', self.balance[self.entertainment] + self.balance[self.clothing] + self.balance[self.food])
         else:
             print('Wrong input, please try again')
             self.computeBalance()
+        option = int(input('Would you like to check any other balance? (1) Yes (2) No \n'))
+        if option == 1:
+            self.computeBalance()
+        else:
+            init()
 
     def transfer(self):
         selectedOption1= input(f'Please enter the category you wish to transfer from ({self.food} or {self.clothing}'
@@ -109,12 +121,45 @@ class Budget:
 
 
 
+def init1():
+    print('Welcome to MyPreciousThreeBudgets')
+    Name = input('What is your name? e.g Samuel \n')
+    print(f'Mr/Mrs {Name} Please enter the three budgets you wish to manage, one space each')
+    Budgets = list(map(str, input().rstrip().split()))
+    global Samuel
+    Samuel = Budget(Budgets[0], Budgets[1], Budgets[2])
+    init()
 
 
 
+def init():
+    print('Please what you like to do? ')
+    print('1. Deposit to a Budget')
+    print('2. Withdraw from a Budget')
+    print('3. Check balances of your Budget categories')
+    print('4. Transfer between Budget categories')
+    print('5. Exit')
 
-Samuel = Budget('Garri', 'Shirt', 'Football')
-Samuel.deposit()
-# Samuel.withdraw()
-#Samuel.computeBalance()
-Samuel.transfer()
+
+    isValidOption = False
+    while isValidOption == False:
+        selectedOption = int(input('Please select an option \n'))
+        if selectedOption == 1:
+            isValidOption = True
+            Samuel.deposit()
+        elif selectedOption == 2:
+            isValidOption = True
+            Samuel.withdraw()
+        elif selectedOption == 3:
+            isValidOption = True
+            Samuel.computeBalance()
+        elif selectedOption == 4:
+            isValidOption = True
+            Samuel.transfer()
+        elif selectedOption == 5:
+            exit()
+        else:
+            print('Wrong input, please try again')
+
+
+init1()
